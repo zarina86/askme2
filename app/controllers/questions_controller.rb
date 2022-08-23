f4ee046736d6898c994c82850540c9b34bb8b5cf
zarina86 <zara86tech@gmail.com>
2022-08-23 22:20:52 +0600

    show, index are added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,16 +2,30 @@ class QuestionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    Question.create(body: params[:question][:body], user_id: params[:question][:user_id])
-  end
-
-  def update
-    @question = Question.find(params[:id])
-    @question.update(body: params[:question][:body], user_id: params[:question][:user_id])
+    Question.create(
+      body: params[:question][:body], 
+      user_id: params[:question][:user_id]
+      )
   end
 
   def destroy
     @question = Question.find(params[:id])
     @question.destroy
   end
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+  
+  def update
+    @question = Question.find(params[:id])
+    @question.update(
+      body: params[:question][:body], 
+      user_id: params[:question][:user_id]
+    )
+  end 
 end
