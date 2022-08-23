7a3ebdab35a714848aaba99d01f6369eb0d47c7d
zarina86 <zara86tech@gmail.com>
2022-08-23 21:39:05 +0600

    questions controller generated, routes modified

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    Question.create(body: params[:question][:body], user_id: params[:question][:user_id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(body: params[:question][:body], user_id: params[:question][:user_id])
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+  end
+end
