f2c9c40b5988b0602cfa024fd7cc49f8b5ded73d
zarina86 <zara86tech@gmail.com>
2022-08-24 20:34:52 +0600

    layouts, assets pipeline, hidden

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,31 +1,51 @@
 class QuestionsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
+  before_action :set_question, only: %i[update destroy edit hide show]
+  
   def create
-    Question.create(
-      body: params[:question][:body], 
-      user_id: params[:question][:user_id]
-      )
+    @question = Question.create(question_params)
+
+    redirect_to question_path(@question)
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
+
+    redirect_to questions_path
+  end
+
+  def edit
+  end
+  
+  def hide
+    @question = Question.update!(hidden: true)
+
+    redirect_to question_path(@question)
   end
 
   def index
     @questions = Question.all
   end
+  
+  def new
+    @question = Question.new
+  end
 
   def show
-    @question = Question.find(params[:id])
   end
   
   def update
-    @question = Question.find(params[:id])
-    @question.update(
-      body: params[:question][:body], 
-      user_id: params[:question][:user_id]
-    )
+    @question.update(question_params)
+
+    redirect_to question_path(@question)
   end 
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body, :user_id)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
 end
