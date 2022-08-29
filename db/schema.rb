fbed97767ec360b22f06054d35121e8eb5d3bc67
zarina86 <zara86tech@gmail.com>
2022-08-29 12:11:32 +0600

    navbar header color selection is added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_28_120215) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_28_141817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,8 +30,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_28_120215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "navbar_color"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
-  
+
 end
