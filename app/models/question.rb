47c3887de3c8836cd552de082e1c03a533d0dd09
zarina86 <zara86tech@gmail.com>
2022-09-04 20:39:40 +0600

    regex is changed

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  VALID_BODY_REGEX = /\A[a-zA-Zа-яА-Я0-9 ,.;'"+-?!%$«»]*(?:\R[а-яА-Я0-9 ,.;'"+-?!%$]*)*\z/.freeze
+  VALID_BODY_REGEX = /\A[«a-zA-Zа-яА-Я0-9 ,.;'"+-?!%$»]*(?:\R[«а-яА-Я0-9 ,.;'"+-?!%$»]*)*\z/.freeze
 
   validates :body, presence: true, length: {maximum: 280}, format: { with: VALID_BODY_REGEX }
   validates :user_id, presence: true
